872b779a291add6991f1d5330fe148079751a723
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2017-10-11 22:49:29 +0800

    Add project files.

---
+++ SimpleBindingFromVM/SimpleBindingFromVM/ViewModels/UsersViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBindingFromVM.ViewModels
+{
+    public class UsersViewModel : INotifyPropertyChanged
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                DisplayName = name;
+                OnPropertyChanged();
+            }
+        }
+
+        private string displayName;
+
+        public string DisplayName
+        {
+            get {
+                return displayName; }
+            set
+            {
+                displayName = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
